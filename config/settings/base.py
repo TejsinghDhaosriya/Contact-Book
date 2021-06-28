0abbd3939b1880bd3c7d6954d00f645ad3cbe65a
Tejsingh Dhaosriya <tejsinghdhaosriya@gmail.com>
2021-06-29 00:18:34 +0530

    [BE]-updated config

--- config/settings/base.py
+++ config/settings/base.py
@@ -274,7 +274,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ['django_filters.rest_framework.DjangoFilterBackend',
                                 'rest_framework.filters.OrderingFilter'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 5
 }
 
 REST_AUTH_SERIALIZERS = {
