f833cce6c4be08bd47e15b9754a04d4167a0cce9
Tejsingh Dhaosriya <tejsinghdhaosriya@gmail.com>
2021-06-29 01:04:50 +0530

    [BE]-updated filter logic

--- workex/apps/api/views.py
+++ workex/apps/api/views.py
@@ -1,12 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
-
+from django_filters import rest_framework as filters
 from workex.apps.models import App
 from workex.apps.api.serializers import AppSerializer      
-      
+
+
+class AppFilter(filters.FilterSet):
+    name = filters.CharFilter(field_name="name",lookup_expr='icontains')
+    class Meta:
+        model=App
+        fields=['name']
+
+
 
 class AppViewSet(ModelViewSet):
     queryset = App.objects.all()
     serializer_class = AppSerializer
-    filter_fields=['name']
+    filterset_class=AppFilter
+    
 
 
